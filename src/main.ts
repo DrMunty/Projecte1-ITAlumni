0275a9fd82fac0a020fecda54252712c53b06cf2
albert-dev <al96@hotmail.es>
2026-05-12 12:38:45 +0200

    feature: "added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import '/.style.css'
 import {Home} from './pages/Home'
 import {Jobs} from './pages/Jobs'
 import {Networking} from './pages/Networking'
+import {setupNavbar} from './components/navbar'
+
 
