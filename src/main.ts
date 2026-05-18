15fe6ed05e4d01194506bf724dc5b3680c1c7c67
albert-dev <al96@hotmail.es>
2026-05-18 10:51:20 +0200

    feature: "added animation, logos and additional styles to navbar buttons"

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
-import './styles/navbar.css';
+import '../styles/navbar.css';
 import { createNavbar } from './components/navbar';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
-    app.innerHTML = createNavbar() + `
-        <main>
-            <section class="hero">
-                <h1>Benvinguts</h1>
-            </section>
-        </main>
-    `;
+    app.innerHTML = createNavbar();
 }
 
 
