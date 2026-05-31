235616284e243c975e811a3c2343d38e95291da3
albert-dev <al96@hotmail.es>
2026-05-31 12:42:18 +0200

    refactor: "fixed jobFilters import route on mobileJobs.ts"

--- src/pages/mobile/mobileJobs.ts
+++ src/pages/mobile/mobileJobs.ts
@@ -1,5 +1,7 @@
 import jobDataRaw from '../../data/jobs.json';
-import type { Job, JobFilters } from '../../components/classes/Job';
+import type { Job } from '../../components/classes/Job';
+import type { JobFilters } from '../../components/classes/JobFilters';
+
 import { filterJobs } from '../../components/global/jobFilterFunction';
 
 const jobData: Job[] = jobDataRaw as Job[];
